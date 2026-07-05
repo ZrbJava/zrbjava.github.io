d4d016e1bf2c019ef2484973df283456ce8c5c32
赵汝波 <rubo_zhao@asdc.com.cn>
2026-07-05 18:57:00 +0800

    🐳 chore: 修改配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import sitemap from "@astrojs/sitemap";
 import rehypeSlug from "rehype-slug";
 import rehypeMermaid from "rehype-mermaid";
 
-const deployTarget = process.env.DEPLOY_TARGET;
-const isGithubPages = deployTarget === "github";
-
 const site =
   process.env.SITE_URL ??
   (process.env.VERCEL_PROJECT_PRODUCTION_URL
@@ -16,11 +13,8 @@ const site =
       ? `https://${process.env.VERCEL_URL}`
       : "https://zrbjava.github.io");
 
-const base = isGithubPages ? (process.env.GITHUB_PAGES_BASE ?? "/blog") : "/";
-
 export default defineConfig({
   site,
-  base,
   integrations: [mdx(), sitemap()],
   markdown: {
     processor: unified({
